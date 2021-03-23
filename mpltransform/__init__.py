db1bae1f579a6ddca5b8bbfa851ca0b1d5cd9fd5
Alastair Garner <alastairgarner@outlook.com>
2021-03-23 21:15:27 +0000

    added core package files

---
+++ mpltransform/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+from .mpltransform import PointTransform, transform_factory, decorator_custom_transform
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mpltransform',
+    version='0.1',
+    description='Custom transforms for matplotlib',
+    url='',
+    author='Alastair G',
+    author_email='garneralastair@gmail.com',
+    license='MIT',
+    packages=['mpltransform'],
+    zip_safe=False
+)
