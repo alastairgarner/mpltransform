7acbbcde6c4ba2535afc2922aa5a36ffc6d7695e
Alastair Garner <alastairgarner@outlook.com>
2021-03-23 21:42:04 +0000

    updated setup with dependencies

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-mpltransform
+# mpltransform
+
+Reference:
+
+- https://python-packaging-tutorial.readthedocs.io/en/latest/setup_py.html
+- https://python-packaging.readthedocs.io/en/latest/minimal.html
--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ setup(
     name='mpltransform',
     version='0.1',
     description='Custom transforms for matplotlib',
-    url='',
+    url='https://github.com/alastairgarner/mpltransform',
     author='Alastair G',
     author_email='garneralastair@gmail.com',
     license='MIT',
     packages=['mpltransform'],
+    install_requires=[
+        'matplotlib',
+    ],
     zip_safe=False
 )
